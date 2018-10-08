ca85831e2619cbfd4be116e57a76f6ea7dd815cb
Oleksandr Krasnoporov <oKrasnoporov@cofcointernational.com>
2018-10-08 15:50:57 +0300

    first steps

---
+++ PassCreation.ps1
@@ -0,0 +1,2 @@
+﻿Read-Host "Enter Password" -AsSecureString | ConvertTo-SecureString `
+-AsPlainText -Force | ConvertFrom-SecureString | Out-File "C:\temp\Password.txt"
---
+++ StartWinInraV01.ps1
@@ -0,0 +1,17 @@
+﻿cls
+Connect-AzureRmAccount
+Save-AzureRmProfile
+
+#Select-AzureRmSubscription
+
+#==========================================================================
+# The azure account here must not be a Live ID.
+
+#$username = "alexbitdevopsroad@outlook.com"
+#$SecurePassword = Get-Content "C:\temp\Password.txt" | ConvertTo-SecureString
+#$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $SecurePassword
+#
+#
+#Login-AzureRmAccount -Credential $cred
+
+#===========================================================================
